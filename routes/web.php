6545e6efbea68362f40391686a364ead41e6bcc2
fiadann <dan.adun25@gmail.com>
2026-05-06 05:38:32 +0700

    pisahin login admin

--- routes/web.php
+++ routes/web.php
@@ -71,17 +71,14 @@ Route::middleware(['auth', 'pelapor'])->group(function () {
         ->name('laporan.my-report');
 
     // PROFILE
-    Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
 
-        Route::get('/profile', [ProfileController::class, 'edit'])
-            ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
 
-        Route::patch('/profile', [ProfileController::class, 'update'])
-            ->name('profile.update');
-
-        Route::delete('/profile', [ProfileController::class, 'destroy'])
-            ->name('profile.destroy');
-    });
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 });
 
 /* =========================
